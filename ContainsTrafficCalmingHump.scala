f99180b90ae248198b89baf02ec50abab5e3d109
Anders Saxrud <brannersrud@gmail.com>
2019-10-17 17:15:53 +0200

    almost 7 fixd

---
+++ ContainsTrafficCalmingHump.scala
@@ -0,0 +1,43 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.functions.{count, explode}
+import org.apache.spark.{SparkConf, SparkContext}
+
+object ContainsTrafficCalmingHump {
+
+  def main(args: Array[String]) {
+    val d = new SparkConf()
+      .setMaster("local")
+      .setAppName("Get address street")
+    val sc = new SparkContext(d)
+
+    sc.setLogLevel("ERROR")
+
+    val spark = SparkSession.builder().getOrCreate()
+    val df = spark.read.format("com.databricks.spark.xml")
+      .option("rootTag", "osm")
+      .option("rowTag", "way")
+      .load("input/areaextract.osm")
+
+
+    val nodedf = spark.read.format("com.databricks.spark.xml")
+      .option("rootTag", "osm")
+      .option("rowTag", "node")
+      .load("input/areaextract.osm")
+
+    import spark.implicits._
+    //Way query for getting the nds
+    val myquery = df.select($"_id".as("WayId") ,$"nd", explode($"nd").as("noderef")).select("noderef._ref", "WayId")
+    //filtering and nodequery
+    val nodeDf = nodedf.select($"_id", explode($"tag").as("Nodetags"))
+    val filteredNodeQuery = nodeDf.filter($"Nodetags._k" === "traffic_calming" && $"Nodetags._v" === "hump")
+    //joining on id
+    val joined = filteredNodeQuery.join(myquery, filteredNodeQuery("_id") === myquery("_ref"))
+    //partition to get count by each id
+    val partition = Window.partitionBy($"WayId")
+    val p = joined.withColumn("barrier_count", count($"Nodetags._k") over partition).orderBy($"barrier_count".desc)
+
+
+    p.show(100)
+  }
+}
